ae7c5f523498258722ee7dbc7a977f6b7184166b
Souptik Karmakar <Souptik Karmakar>
2018-07-09 14:33:15 +0530

    eror fix

--- src/main/java/com/em/service/BannerServiceImpl.java
+++ src/main/java/com/em/service/BannerServiceImpl.java
@@ -6,11 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-
-import static com.sun.org.apache.xml.internal.security.keys.keyresolver.KeyResolver.iterator;
 
 /**
  * Created by ANIMUS on 22-08-2017.
